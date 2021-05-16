97902e93821423e548f443b1b11e902d819cd9d5
Richeir <315310840@qq.com>
2021-05-16 18:08:38 +0800

    20210516

--- hello.go
+++ hello.go
@@ -2,15 +2,32 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == ""{
 		name = "world"
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(name, language)
+}
+
+func greetingPrefix(name string, language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix + name
+	case french:
+		prefix = frenchHelloPrefix + name
+	default:
+		prefix = englishHelloPrefix + name
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
