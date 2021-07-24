2d11d7940b5bf4c7081a7ade1ebd30aa4f302b36
Richeir <richeir@outlook.com>
2021-07-24 21:33:06 +0800

    20210724

---
+++ test/wallet/wallet.go
@@ -0,0 +1,25 @@
+package wallet
+
+import "fmt"
+
+type Bitcoin int
+
+type Stringer interface {
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposite(amount Bitcoin) {
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
---
+++ test/wallet/wallet_test.go
@@ -0,0 +1,17 @@
+package wallet
+
+import (
+	"testing"
+)
+
+func TestWallet(t *testing.T) {
+	wallet := Wallet{}
+	wallet.Deposite(Bitcoin(10))
+	got := wallet.Balance()
+
+	want := Bitcoin(20)
+
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
+}
